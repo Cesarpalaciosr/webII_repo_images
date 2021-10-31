9a6a2d0ae388a4f240625fde037430f1dbd20547
CesarP <capr99@hotmail.com>
2021-10-30 20:37:53 -0400

    de locos

--- src/main/java/cp/controllers/ConnectionDB.java
+++ src/main/java/cp/controllers/ConnectionDB.java
@@ -11,14 +11,14 @@ Connection conn = null;
 	public ConnectionDB() {
 		
 		String driver = "org.postgresql.Driver";
-/*		
 		String url = "jdbc:postgresql://localhost:5432/dbprueba";
 		String user = "postgres";
 		String password = "masterkey";
-*/
+		/*		
 		String url = "jdbc:postgresql://ec2-3-222-11-129.compute-1.amazonaws.com:5432/de511gfgus123n?sslmode=require";
 		String user = "tryqteclkftanp";
 		String password = "05db7b52ac66289538be68d886f33753a8839d2de5c81f0272ec20f8760a8fd8" ;
+ */
 	
 		try {
 			Class.forName(driver);
